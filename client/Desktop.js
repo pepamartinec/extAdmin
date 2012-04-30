9788874e5e9ea0ead5bdb57ae374c2443cf736e8
joker806 <joker806@gmail.com>
2012-04-30 22:48:25 +0200

    Disable the debug panel

--- client/Desktop.js
+++ client/Desktop.js
@@ -1,7 +1,7 @@
 Ext.define( 'extAdmin.Desktop',
 {
 	extend : 'Ext.container.Viewport',
-	
+
 	requires : [
 		'Ext.layout.container.Border',
 		'Ext.view.View',
@@ -9,7 +9,7 @@ Ext.define( 'extAdmin.Desktop',
 		'Ext.layout.container.Fit',
 		'extAdmin.Store'
 	],
-	
+
 	uses : [
 		//<debug>
 		'extAdmin.debug.ConsolePanel',
@@ -17,122 +17,122 @@ Ext.define( 'extAdmin.Desktop',
 		'extAdmin.debug.ModulesCachePanel'
 		//</debug>
 	],
-	
+
 	/**
 	 * @config {extAdmin.Environment}
 	 */
 	env : null,
-	
+
 	/**
 	 * @private
 	 * @property {extAdmin.Module}
 	 */
 	activeModule : null,
-	
+
 	/**
 	 * @private
 	 * @property {Ext.AbstractComponent}
 	 */
 	activeComponent : null,
-	
+
 	/**
 	 * Desktop constructor
-	 * 
+	 *
 	 * @param config
 	 */
 	constructor : function( config )
 	{
 		var me = this;
-		
+
 		me.env = config.env;
-		
+
 		me.modulesPanel = Ext.create( 'Ext.view.View', {
 			region     : 'west',
 			width      : 125,
 			autoScroll : true,
-			
+
 			componentCls : 'x-modules-list',
 			itemCls      : 'module',
 			itemTpl      : [
 				'{name}'
 			],
-			
+
 			store : me.env.createStore({
 				loadAction : [ '\\ExtAdmin\\Module\\SystemModule', 'getMenuItems' ],
-				
+
 				autoLoad : true,
 				fields   : [ 'name', 'entryModule' ]
 			}),
-			
+
 			listeners    : {
 				itemclick : function( view, module ) {
 					me.openModule( module.get( 'entryModule' ) );
 				}
 			}
 		});
-		
+
 		me.contentPanel = Ext.create( 'Ext.container.Container', {
 			region : 'center',
 			layout : 'fit'
-		});		
-		
-		me.debugPanel = Ext.create( 'Ext.tab.Panel', {
-			region      : 'south',
-			title       : 'Debug panel',
-			split       : true,
-			collapsible : true,
-			height      : 200,
-			
-			items  : [
-				Ext.create( 'extAdmin.debug.ConsolePanel', {
-					env : me.env
-				}),
-			          
-				Ext.create( 'extAdmin.debug.CommunicationPanel', {
-					env : me.env
-				}),
-				
-				Ext.create( 'extAdmin.debug.ModulesCachePanel', {
-					env : me.env
-				}),				
-			]
 		});
-		
+
+//		me.debugPanel = Ext.create( 'Ext.tab.Panel', {
+//			region      : 'south',
+//			title       : 'Debug panel',
+//			split       : true,
+//			collapsible : true,
+//			height      : 200,
+//
+//			items  : [
+//				Ext.create( 'extAdmin.debug.ConsolePanel', {
+//					env : me.env
+//				}),
+//
+//				Ext.create( 'extAdmin.debug.CommunicationPanel', {
+//					env : me.env
+//				}),
+//
+//				Ext.create( 'extAdmin.debug.ModulesCachePanel', {
+//					env : me.env
+//				}),
+//			]
+//		});
+
 		Ext.apply( me, {
 			layout : 'border',
-			items  : [ me.modulesPanel, me.contentPanel, me.debugPanel ]
+			items  : [ me.modulesPanel, me.contentPanel ]
 		});
-		
+
 		me.callParent( arguments );
 	},
-	
+
 	openModule : function( module )
 	{
 		var me   = this,
 		    view = null;
-		
+
 //		try {
 			// launch new module
 			module = me.env.moduleManager.get( module );
 			view   = module.createView();
-			
+
 //		} catch( e ) {
 //			Ext.Error.raise({
 //				msg   : e.message,
 //				stack : e.stack
 //			});
 //		}
-		
-		// if new view was successfuly created 
+
+		// if new view was successfuly created
 		if( view ) {
 			// remove old module
 			if( me.activeComponent ) {
 				me.contentPanel.remove( me.activeComponent, true );
 			}
-			
+
 			// display new module
 			me.contentPanel.add( view );
-			
+
 			me.activeModule    = module;
 			me.activeComponent = view;
 		}
